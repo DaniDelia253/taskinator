f4afcf9de24f3c66eea67a9e24f6262d7d235519
Danielle Davis <danidavis321@gmail.com>
2021-12-11 10:11:28 -0600

    add delete functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector('#page-content');
 
 var taskFormHandler = function (event) {
     event.preventDefault();
@@ -48,7 +49,7 @@ var createTaskEl = function (taskDataObj) {
     tasksToDoEl.appendChild(listItemEl);
 
     //increase counter for next unique id
-    taskIdCounter ++;
+    taskIdCounter++;
 };
 
 var createTaskActions = function (taskId) {
@@ -79,7 +80,7 @@ var createTaskActions = function (taskId) {
     actionContainerEl.appendChild(statusSelectEl);
 
     var statusChoices = ["To Do", "In Progress", "Completed"];
-    for (i = 0; i <statusChoices.length; i++) {
+    for (i = 0; i < statusChoices.length; i++) {
         //create option element
         var statusOptionEl = document.createElement("option");
         statusOptionEl.textContent = statusChoices[i];
@@ -93,3 +94,20 @@ var createTaskActions = function (taskId) {
 };
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function (event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        //get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+}
+
+pageContentEl.addEventListener("click", taskButtonHandler);
